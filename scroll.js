0a84301b4d5fa1732aa9f28463412a2edaaa8e34
Santhosh1203122 <rm.santhosh1234@gmail.com>
2018-01-15 00:48:53 -0800

    Latest update

---
+++ scroll.js
@@ -0,0 +1,38 @@
+$("#down").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#aboutus").offset().top
+                },
+                'slow');
+        });
+        
+        $("#aboutbutton").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#aboutus").offset().top
+                },
+                'slow');               
+        });
+       
+       $("#servicebutton").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#services").offset().top
+                },
+                'slow');               
+        });
+        $("#productbutton").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#product").offset().top
+                },
+                'slow');               
+        });
+        $("#portfoliobutton").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#portfolio").offset().top
+                },
+                'slow');               
+        });
+        $("#contactbutton").click(function() {
+            $('html,body').animate({
+                    scrollTop: $("#contact").offset().top
+                },
+                'slow');               
+        });
